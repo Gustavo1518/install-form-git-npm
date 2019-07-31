97eabe7c3a2afa300d829d4a6ce1a697550d6ba9
Jóvenes Construyendo el Futuro 07 <jovenes.cf07@conacyt.mx>
2019-07-31 11:09:29 -0500

    se creo repositorio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>FORMULARIO CONACYT</h1>
+    <form-conacyt></form-conacyt>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+
+import { HelloWorldComponent } from "@conacyt/hello-world/src";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
-})
+components: {"form-conacyt": HelloWorldComponent}
+  })
 export default class Home extends Vue {}
 </script>
